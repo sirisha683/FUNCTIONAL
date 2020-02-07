3fae069e60226e7c2ea4f703743f19f24b49ec36
sirisha683 <sirisha.sharaba@gmail.com>
2020-02-07 15:57:50 -0800

    mavenproject

---
+++ learntestng/src/test/java/com/learntestng/AppTest.java
@@ -0,0 +1,18 @@
+package com.learntestng;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+      System.out.println("test1");	
+    }
+}
---
+++ learntestng/src/test/java/com/learntestng/SampleTest.java
@@ -0,0 +1,13 @@
+package com.learntestng;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test2()
+	{
+		
+		System.out.println("****************test2********");
+	}
+}
